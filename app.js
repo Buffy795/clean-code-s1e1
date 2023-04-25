80c06e9f294c64a741811d1b15aa2d1fde72b342
Vlada Makeeva <vlada0795@gmail.com>
2023-04-25 17:02:12 +0300

    fix(basics-1.2): put low register

--- app.js
+++ app.js
@@ -90,16 +90,18 @@ var editTask=function(){
     var label=listItem.querySelector(".task__label");
     var editBtn=listItem.querySelector(".task__edit-btn");
     var containsClass=document.querySelector(".task-edit");
+    
     //If class of the parent is .editmode
     if(containsClass) {
 
         //switch to .editmode
         //label becomes the inputs value.
+        
         label.innerText = editInput.value;
-        editBtn.textContent = "Edit";
+        editBtn.innerText = "Edit";
     } else {
         editInput.value = label.innerText;
-        editBtn.textContent = "Save";
+        editBtn.innerText = "Save";
     }
 
     //toggle .editmode on the parent.
